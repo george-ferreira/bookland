ad6cd0165448bc9016e1a03107a677f779435007
George Ferreira <jogosdogeorge@gmail.com>
2024-09-02 21:35:10 -0300

    Atualizando o migration

--- bookland/Program.cs
+++ bookland/Program.cs
@@ -1,4 +1,5 @@
 using bookland;
+using bookland.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
--- bookland/obj/Debug/net8.0/bookland.AssemblyInfo.cs
+++ bookland/obj/Debug/net8.0/bookland.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bookland")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f439601a9cae45e0923cc96f6f0337bbcb2503")]
 [assembly: System.Reflection.AssemblyProductAttribute("bookland")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bookland")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
