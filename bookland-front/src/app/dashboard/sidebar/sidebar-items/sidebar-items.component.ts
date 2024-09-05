4a3a5ebf4fcf4e3a85640a936a1c212207f3a1d5
George Ferreira <jogosdogeorge@gmail.com>
2024-09-05 20:11:51 -0300

    Removendo projetos antigos

--- bookland-front/src/app/dashboard/sidebar/sidebar-items/sidebar-items.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sidebar-items',
-  templateUrl: './sidebar-items.component.html'
-})
-export class SidebarItemsComponent {
-
-}
